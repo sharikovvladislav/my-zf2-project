d0dfc060b880dec6e5af3ae202aa376c527bdc47
sharikovvladislav <sharikov.vladislav@gmail.com>
2014-04-30 17:12:16 +0400

    Initial commit

---
+++ module/ZfcUserOverride/config/module.config.php
@@ -0,0 +1,26 @@
+<?php
+$config = array(
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'zfcuser' => __DIR__ . '/../view',
+        ),
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'zfcuser' => 'ZfcUserOverride\Controller\UserController',
+        ),
+    )
+);
+
+$config['router']['routes']['zfcuser']['child_routes']['trailing_slash'] = array(
+    'type' => 'Literal',
+    'options' => array(
+        'route' => '/',
+        'defaults' => array(
+            'controller' => 'zfcuser',
+            'action'     => 'index',
+        ),
+    ),
+);
+
+return $config;
