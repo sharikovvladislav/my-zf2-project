ae76c7e1cf5a0b51a0120a713d8157ff7a3708cd
sharikovvladislav <sharikov.vladislav@gmail.com>
2014-05-12 18:29:36 +0400

    'updated'

--- module/MyZfcAdmin/config/module.config.php
+++ module/MyZfcAdmin/config/module.config.php
@@ -1,6 +1,6 @@
 <?php
 
-return array(
+$config = array(
     'controllers' => array(
         'invokables' => array(
             'ZfcAdmin\Controller\AdminController' => 'MyZfcAdmin\Controller\AdminController',
@@ -12,3 +12,65 @@ return array(
         ),
     ),
 );
+
+$config['router']['routes']['zfcadmin']['child_routes'] = array(
+    'news' => array(
+        'type' => 'literal',
+        'options' => array(
+            'route' => '/news',
+            'defaults' => array(
+                'controller' => 'News\Controller\NewsItem',
+                'action'     => 'index',
+            ),
+            'may_terminate' => true, 
+            'child_routes' => array(
+                'addsome' => array(
+                    'type' => 'literal',
+                    'options' => array(
+                        'route' => '/add',
+                        'defaults' => array(
+                            'controller' => 'News\Controller\NewsItem',
+                            'action'     => 'add',
+                        ),
+                        'may_terminate' => true, 
+                    ),
+                ),
+            ),
+        ),
+    ),
+);
+
+
+/*
+    'router' => array(
+        'routes' => array(
+            'news' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/news[/][:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'News\Controller\NewsItem',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+            'add news item' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/news/add',
+                    'defaults' => array(
+                        'controller' => 'News\Controller\NewsItem',
+                        'action'     => 'add',
+                    ),
+                ),
+            ),
+        ),
+    ),
+
+*/
+
+return $config;
