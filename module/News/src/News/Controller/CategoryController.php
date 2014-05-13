e3b029e05ab1063d2b28f6bc085aa6c31842e763
sharikovvladislav <sharikov.vladislav@gmail.com>
2014-05-14 03:58:53 +0400

    add/delete action finished

--- module/News/src/News/Controller/CategoryController.php
+++ module/News/src/News/Controller/CategoryController.php
@@ -4,17 +4,16 @@ namespace News\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
-use \News\Entity\NewsItem as NewsItem;
-use \News\Form\AddNewsForm as AddNewsForm;
+use \News\Entity\Category as Category;
+use \News\Form\AddCategoryForm as AddCategoryForm;
 
 class CategoryController extends AbstractActionController {
     public function indexAction() {
         $objectManager = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
-
         $categories = $objectManager
             ->getRepository('\News\Entity\Category')
             ->findAll();
-        var_dump($categories);
+            
         $items = array();
         foreach ($categories as $category) {
             $items[] = $category->getArrayCopy();
@@ -26,60 +25,53 @@ class CategoryController extends AbstractActionController {
 
         return $view;
     }
-    public function listAction() {
-
-        $objectManager = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
-
-        $news = $objectManager
-            ->getRepository('\News\Entity\Item')
-            ->findBy(array(), array('created' => 'DESC'));
-
-        $items = array();
-        foreach ($news as $item) {
-            $items[] = $item->getArrayCopy();
-        }
-
-        $view = new ViewModel(array(
-            'news' => $items,
-        ));
-
-        return $view;
-    }
 
     public function addAction() {
-        $form = new AddNewsForm();
-        $form->get('submit')->setValue('Add');
-        
+        $form = new AddCategoryForm();
+        $form->get('visible')->setCheckedValue(0);
+        $form->get('visible')->setUncheckedValue(1);
         $request = $this->getRequest();
         if ($request->isPost()) {
-        
             $form->setData($request->getPost());
-            //var_dump($form->isValid());
-            //var_dump($form->getMessages()); //error messages
-            //var_dump($form->getInputFilter()->getMessages());
             if ($form->isValid()) {
                 $objectManager = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
 
-                $item = new NewsItem();
+                $item = new Category();
 
                 $item->exchangeArray($form->getData());
-
-                $item->setCreated(time());
-                $item->setUserId(0);
-                $item->setCategoryId(0);
 
                 $objectManager->persist($item);
                 $objectManager->flush();
 
                 // Redirect to list of blogposts
-                return $this->redirect()->toRoute('news');
+                return $this->redirect()->toRoute('zfcadmin/news/categories');
             }
         }
         return array('form' => $form);
     }
 
     public function deleteAction() {
-        return new ViewModel();
+        $id = (int)$this->params('id');
+        $entityManager = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+        $category = $entityManager
+            ->getRepository('\News\Entity\Category')
+            ->findOneById($id);
+        
+        if(isset($category)) {
+            $result = array(
+                'result' => true,
+                'categoryName' => $category->getName(),
+            );
+            
+            $entityManager->remove($category);
+            $entityManager->flush();
+        } else {
+            $result = array(
+                'result' => false,
+            );
+        }
+        $view = new ViewModel($result);
+        return $view;
     }
 
     public function editAction(){
