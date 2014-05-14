f17c5d0634d3b07c9ed79ac95e41f37e77034413
sharikovvladislav <sharikov.vladislav@gmail.com>
2014-05-14 04:01:25 +0400

    renamed form => updated files

---
+++ module/News/src/News/Form/AddNewsItemInputFilter.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace News\Form;
+
+use Zend\Form\Form;
+use Zend\InputFilter\InputFilter;
+
+class AddNewsItemInputFilter extends InputFilter
+{
+    public function __construct()
+    {
+        $this->add(array(
+            'name' => 'title',
+            'required' => true,
+            'validators' => array(
+                array(
+                    'name' => 'StringLength',
+                    'options' => array(
+                        'min' => 3,
+                        'max' => 100,
+                    ),
+                ),
+            ),
+            'filters' => array(
+                array('name' => 'StripTags'),
+                array('name' => 'StringTrim'),
+            ),
+
+        ));
+
+        $this->add(array(
+            'name' => 'text',
+            'required' => true,
+            'validators' => array(
+                array(
+                    'name' => 'StringLength',
+                    'options' => array(
+                        'min' => 5,
+                    ),
+                ),
+            ),
+            'filters' => array(
+                array('name' => 'StripTags'),
+                array('name' => 'StringTrim'),
+            ),
+        ));
+        $this->add(array(
+            'name' => 'state',
+            'required' => true,
+        ));
+        $this->add(array(
+            'name' => 'security',
+            'required' => false,
+        ));
+    }
+}
