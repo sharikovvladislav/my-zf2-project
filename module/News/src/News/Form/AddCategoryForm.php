6faa21cb4b861e70a248343356e0d93f6087a6ee
sharikovvladislav <sharikov.vladislav@gmail.com>
2014-05-14 04:01:40 +0400

    added form for adding category

---
+++ module/News/src/News/Form/AddCategoryForm.php
@@ -0,0 +1,53 @@
+<?php
+namespace News\Form;
+
+use Zend\Form\Form;
+use Zend\InputFilter\Factory as InputFactory;
+use Zend\InputFilter\InputFilter;
+use \News\Form\AddNewsItemInputFilter as AddNewsItemInputFilter;
+
+class AddCategoryForm extends Form
+{
+    public function __construct($name = null)
+    {
+        parent::__construct('categoryAdd');
+        $this->setAttribute('method', 'post');
+        $this->setInputFilter(new AddCategoryInputFilter());
+        $this->add(array(
+            'name' => 'security',
+            'type' => 'Zend\Form\Element\Csrf',
+        ));
+        $this->add(array(
+            'name' => 'id',
+            'type' => 'Hidden',
+        ));
+        $this->add(array(
+            'name' => 'created',
+            'type' => 'Hidden',
+        ));
+        $this->add(array(
+            'name' => 'name',
+            'type' => 'Text',
+            'options' => array(
+                'min' => 3,
+                'max' => 30,
+                'label' => 'Название',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'visible',
+            'type' => 'Checkbox',
+            'options' => array(
+                'label' => 'скрывать новости в категории',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'submit',
+            'type' => 'Submit',
+            'attributes' => array(
+                'value' => 'Добавить',
+                'id' => 'submitbutton',
+            ),
+        ));
+    }
+}
---
+++ module/News/src/News/Form/AddCategoryInputFilter.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace News\Form;
+
+use Zend\Form\Form;
+use Zend\InputFilter\InputFilter;
+
+class AddCategoryInputFilter extends InputFilter
+{
+    public function __construct()
+    {
+        $this->add(array(
+            'name' => 'name',
+            'required' => true,
+            'validators' => array(
+                array(
+                    'name' => 'StringLength',
+                    'options' => array(
+                        'min' => 3,
+                        'max' => 30,
+                    ),
+                ),
+            ),
+            'filters' => array(
+                array('name' => 'StripTags'),
+                array('name' => 'StringTrim'),
+            ),
+
+        ));
+        $this->add(array(
+            'name' => 'security',
+            'required' => false,
+        ));
+    }
+}
