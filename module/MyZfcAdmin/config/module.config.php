fb4811a6ea0d6fc9a50e90ccdd396af9ee33807e
sharikovvladislav <sharikov.vladislav@gmail.com>
2014-05-16 03:39:13 +0400

    fixed routes + add layout/login

--- module/MyZfcAdmin/config/module.config.php
+++ module/MyZfcAdmin/config/module.config.php
@@ -9,11 +9,49 @@ $config = array(
     'view_manager' => array(
         'template_path_stack' => array(
             __DIR__ . '/../view'
+        ),        
+        'template_map' => array(
+            'layout/login'           => __DIR__ . '/../view/layout/login.phtml',
         ),
     ),
 );
 
+$config['router']['routes']['zfcadmin'] = array(
+    'type' => 'Segment',
+    'options' => array(
+        'route'    => '/admin',
+        'constraints' => array(
+            'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+        ),
+        'defaults' => array(
+            'controller' => 'ZfcAdmin\Controller\AdminController',
+            'action'     => 'index',
+        ),
+    ),
+    'may_terminate' => true,
+);
+
 $config['router']['routes']['zfcadmin']['child_routes'] = array(
+    'login' => array(
+        'type'    => 'Literal',
+        'options' => array(
+            'route'    => '/login',
+            'defaults' => array(
+                'controller' => 'ZfcAdmin\Controller\AdminController',
+                'action'     => 'login',
+            ),
+        ),
+    ),
+    'logout' => array(
+        'type'    => 'Literal',
+        'options' => array(
+            'route'    => '/logout',
+            'defaults' => array(
+                'controller' => 'ZfcAdmin\Controller\AdminController',
+                'action'     => 'logout',
+            ),
+        ),
+    ),
     'news' => array(
         'type' => 'Literal',
         'options' => array(
